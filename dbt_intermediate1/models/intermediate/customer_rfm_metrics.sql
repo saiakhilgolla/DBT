1568f233b4098c656aafd4d4292d03dd24a042c3
Sai Akhil Golla <saiakhil1011@gmail.com>
2024-02-27 16:56:03 -0500

    fixed issue in the intermediate model query

--- dbt_intermediate1/models/intermediate/customer_rfm_metrics.sql
+++ dbt_intermediate1/models/intermediate/customer_rfm_metrics.sql
@@ -1,12 +1,12 @@
-WITH source_data as(
+WITH source_data AS (
 	SELECT
 		customer_id,
 		COUNT(DISTINCT t0.order_id) AS total_orders,
 		AVG(t1.order_value) AS avg_order_value,
-		MAX(order_purchase_date)) as last_purchase_date
-	FROM {{ref('stg_bigquery_orders')}} as t0
-	INNER JOIN {{ref('stg_bigquery_order_items')}} as t1
+		MAX(order_purchase_date) AS last_purchase_date
+	FROM {{ref('stg_bigquery_orders')}} AS t0
+	INNER JOIN {{ref('stg_bigquery_order_items')}} AS t1
 	ON t0.order_id = t1.order_id
 	GROUP BY customer_id
 )
-select * from source_data
+SELECT * FROM source_data
