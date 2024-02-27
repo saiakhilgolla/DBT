89c920c48837241ee036eba58e35118d488056e0
Sai Akhil Golla <saiakhil1011@gmail.com>
2024-02-27 17:10:54 -0500

    conveted days since last puchase to days. updated datediff formula to one that complies with BQ syntax

--- dbt_intermediate1/models/intermediate/customer_rfm_metrics.sql
+++ dbt_intermediate1/models/intermediate/customer_rfm_metrics.sql
@@ -3,7 +3,7 @@ WITH source_data AS (
 		customer_id,
 		COUNT(DISTINCT t0.order_id) AS total_orders,
 		AVG(t1.order_value) AS avg_order_value,
-		MIN(t0.max_date - order_purchase_date) AS days_since_last_purchase
+		MIN(DATE_DIFF(t0.max_date, order_purchase_date, DAY )) AS days_since_last_purchase
 	FROM {{ref('stg_bigquery_orders')}} AS t0
 	INNER JOIN {{ref('stg_bigquery_order_items')}} AS t1
 	ON t0.order_id = t1.order_id
